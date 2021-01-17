b2e2f4fb1870d0719cc4338a620b7fd9a0b37e1b
rockz637 <gokulrockz637@gmail.com>
2021-01-17 14:08:56 +0530

    s

--- src/com/company/CommonBehave.java
+++ src/com/company/CommonBehave.java
@@ -1,8 +1,11 @@
 package com.company;
 
+import com.sun.xml.internal.bind.v2.runtime.output.StAXExStreamWriterOutput;
+
 public class CommonBehave {
-    int apID, noOP;
+    int apID, noOP,vp,rp ,cp;
     float price;
+    float version,ramEfficieny,compatabilityScore;
     CommonBehave(){
         System.out.println("BILL");
 
@@ -16,7 +19,86 @@ public class CommonBehave {
         System.out.println("ID ="+apID+"\nNumber of modules= "+noOP+"\nCost of a single module "+price);
     }
     void Calculation(){
-        System.out.println("Total cost = "+noOP*price);
+        System.out.println("Total cost = "+(noOP*(price+ vp + rp+cp)));
     }
+    void setFeaturesAndroid(float version,float ramEfficieny,float compatabilityScore){
+        this.version=version;
+        this.ramEfficieny=ramEfficieny;
+        this.compatabilityScore=compatabilityScore;
+    }
+    void versionPriceAndroid(){
+        if(version >= 1 && version<=5){
+            vp=10000;
+        }
+        else if (version>=6 && version<=10){
+            vp=20000;
+
+        }
+        else if (version == 11) {
+            vp=30000;
+        }
+
+
+    }
+    void ramPriceAndroid(){
+        if(ramEfficieny<=80){
+            rp =20000;
+        }
+        else if(ramEfficieny>=81){
+            rp =30000;
+        }
+
+    }
+    void priceCompatabilityScore(){
+        if(compatabilityScore>=1&&compatabilityScore<=5){
+            cp=30000;
+        }
+        else if (compatabilityScore>=6&&compatabilityScore<=10){
+            cp= 40000;
+        }
+
+    }
+    void diplayPriceFeatures(){
+        System.out.println("Version Price: "+vp+" x "+noOP+"\nRam Efficieny Price: "+rp+" x "+noOP+"\nCompatability price: "+cp+" x "+noOP);
+
+    }
+    void setFeaturesIos(float version,float ramEfficieny,float compatabilityScore){
+        this.version=version;
+        this.ramEfficieny=ramEfficieny;
+        this.compatabilityScore=compatabilityScore;
+    }
+    void versionPriceIos(){
+        if(version >= 1 && version<=5){
+            vp=20000;
+        }
+        else if (version>=6 && version<=10){
+            vp=30000;
+
+        }
+        else if (version == 11) {
+            vp=40000;
+        }
+
+
+    }
+    void ramPriceIos(){
+        if(ramEfficieny<=80){
+            rp =20000;
+        }
+        else if(ramEfficieny>=81){
+            rp =25000;
+        }
+
+    }
+    void priceCompatabilityIos(){
+        if(compatabilityScore>=1&&compatabilityScore<=5){
+            cp=40000;
+        }
+        else if (compatabilityScore>=6&&compatabilityScore<=10){
+            cp= 50000;
+        }
+
+    }
+
 
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -22,18 +22,40 @@ public class Main extends CommonBehave {
     public void Android () {
         System.out.println("no of modules=");
         int NoA=scan.nextInt();
+        System.out.println("Enter the verion from 1 to 11");
+        int Ve = scan.nextInt();
+        System.out.println("Enter the ram efficency from 70 to 99 ");
+        int Re = scan.nextInt();
+        System.out.println("Enter the compatability score 1 to 10");
+        int Cs = scan.nextInt();
         CommonBehave Android = new CommonBehave();
+        Android.setFeaturesAndroid(Ve,Re,Cs);
+        Android.versionPriceAndroid();
+        Android.ramPriceAndroid();
+        Android.priceCompatabilityScore();
         Android.apID=1;
         Android.setCommonBehave(1,NoA,60000);
         Android.displayCommonBehave();
+        Android.diplayPriceFeatures();
         Android.Calculation();
     }
     public void Ios(){
         System.out.println("no of modules=");
         int NoI= scan.nextInt();
+        System.out.println("Enter the verion from 1 to 11");
+        int Ve = scan.nextInt();
+        System.out.println("Enter the ram efficency from 70 to 99 ");
+        int Re = scan.nextInt();
+        System.out.println("Enter the compatability score 1 to 10");
+        int Cs = scan.nextInt();
         CommonBehave Ios = new CommonBehave();
+        Ios.setFeaturesIos(Ve,Re,Cs);
+        Ios.versionPriceIos();
+        Ios.ramPriceIos();
+        Ios.priceCompatabilityIos();
         Ios.setCommonBehave(2,NoI,90000);
         Ios.displayCommonBehave();
+        Ios.diplayPriceFeatures();
         Ios.Calculation();
     }
 }
